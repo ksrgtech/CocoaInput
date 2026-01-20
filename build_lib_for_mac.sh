3a1482ea575079e415ffcc56303d94ac524f7f71
Kisaragi Marine <kisaragi.effective@gmail.com>
2026-01-20 11:19:04 +0900

    build(native): add more information on each platform-build

--- build_lib_for_mac.sh
+++ build_lib_for_mac.sh
@@ -13,6 +13,12 @@ fi
 # shellcheck disable=SC2034
 # shellcheck disable=SC2155
 export GIT_ROOT="$(git rev-parse --show-toplevel)"
+if [ -z "$GIT_ROOT" ]; then
+  die "Requires git to be installed"
+fi
+
+echo "Minecraft version: $VERSION"
+echo "Git repository root: $GIT_ROOT"
 
 mkdir -p "$VERSION/src/main/resources/darwin"
 cd "$(dirname "$0")/libcocoainput/darwin/libcocoainput" || die "cd failed"
